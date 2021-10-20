c59c6abf7535b0e950a8c6b1519a4da29012bd82
Donghoon Kim <fly33499@gmail.com>
2021-10-20 16:19:29 +0900

    fix: change upgrade handler for v0.2.9

--- app/app.go
+++ app/app.go
@@ -88,8 +88,6 @@ import (
 	tmos "github.com/tendermint/tendermint/libs/os"
 	dbm "github.com/tendermint/tm-db"
 
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
-
 	"github.com/tendermint/spm/cosmoscmd"
 	"github.com/tendermint/spm/openapiconsole"
 
@@ -235,34 +233,39 @@ func (app *App) registerUpgradeHandlers() {
 	const newVersionName = "v0.2.9"
 
 	app.UpgradeKeeper.SetUpgradeHandler(newVersionName, func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
-		// 1st-time running in-store migrations, using 1 as fromVersion to
-		// avoid running InitGenesis.
+
+		// module version of v0.2.8 below
+
 		fromVM := map[string]uint64{
-			"auth":         1,
-			"bank":         1,
+			"auth":         2,
+			"authz":        1,
+			"bank":         3,
 			"capability":   1,
 			"crisis":       1,
-			"distribution": 1,
+			"distribution": 2,
 			"evidence":     1,
-			"gov":          1,
+			"gov":          2,
 			"mint":         1,
 			"params":       1,
-			"slashing":     1,
-			"staking":      1,
+			"slashing":     2,
+			"staking":      3,
 			"upgrade":      1,
 			"vesting":      1,
 			"ibc":          1,
 			"genutil":      1,
 			"transfer":     1,
+			"feegrant":     1,
 		}
 
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	})
 
-	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	// in case of new module added first
+	/*upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
 		panic(err)
 	}
+
 
 	if upgradeInfo.Name == newVersionName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{
@@ -271,7 +274,7 @@ func (app *App) registerUpgradeHandlers() {
 
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
-	}
+	}*/
 }
 
 // New returns a reference to an initialized Gaia.
