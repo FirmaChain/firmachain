ad5a7dc64d1b8fa2ca8c458fb4f65e079401a69a
Donghoon Kim <fly33499@gmail.com>
2022-05-27 15:06:18 +0900

    fix: "app.configurator is nil" when upgrade

--- app/app.go
+++ app/app.go
@@ -690,7 +690,9 @@ func New(
 
 	app.mm.RegisterInvariants(&app.CrisisKeeper)
 	app.mm.RegisterRoutes(app.Router(), app.QueryRouter(), encodingConfig.Amino)
-	app.mm.RegisterServices(module.NewConfigurator(app.appCodec, app.MsgServiceRouter(), app.GRPCQueryRouter()))
+
+	app.configurator = module.NewConfigurator(app.appCodec, app.MsgServiceRouter(), app.GRPCQueryRouter())
+	app.mm.RegisterServices(app.configurator)
 
 	// create the simulation manager and define the order of the modules for deterministic simulations
 	app.sm = module.NewSimulationManager(
