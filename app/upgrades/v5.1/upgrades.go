042e5cc654e98027f89bbd606f747e8fe29dbe6c
haryunio <hr4574@naver.com>
2025-11-06 09:55:29 +0900

    test: apply presets for the testnet testing

--- app/upgrades/v5.1/upgrades.go
+++ app/upgrades/v5.1/upgrades.go
@@ -18,17 +18,8 @@ import (
 )
 
 var OldValidators = []string{
-	"firmavaloper1p90hu6pqd57xgdauf0l8dwpau73jkk7273y6ck",
-	"firmavaloper1x0lqg5vcynse3r6mug8vteu77cqyaqkgw2rg3x",
 	"firmavaloper1cztkfn8alkj69d79hvygvdueuvyxem7zvu2s9e",
 	"firmavaloper1vqr622jkes6xmh2cxqa7a04dp6seyme3gkxml4",
-	"firmavaloper1pa8yzyytgfg5hk75gmhkpcq6kds57ak39ydw8v",
-	"firmavaloper1d84pmnumnsh80v74lta0vnpd476ncp4pvqhdld",
-	"firmavaloper1u7f4kz740jcr5skq9xnpw8568uugtja5u0k87v",
-	"firmavaloper1paujadj4fxmqrzwrnhqacuk5dst3fkhc5gwddu",
-	"firmavaloper16treuwa6d64xs4r4sshurw75363pwp4n86s6sq",
-	"firmavaloper1q90jttne2mje828mfh3nmfw8kucdsfwf6a5c20",
-	"firmavaloper1tjc2j8aq59s2t9waakacj9xc76yx97t7c55hkh",
 }
 
 type AccountMove struct {
@@ -39,59 +30,14 @@ type AccountMove struct {
 
 var Moves = []AccountMove{
 	{
-		OldAccStr: "firma1p90hu6pqd57xgdauf0l8dwpau73jkk72qz0pcc",
-		NewAccStr: "firma1k0m54qycp4v04wazj0f72snp86htau5g6ujfau",
-		NewValStr: "firmavaloper1k0m54qycp4v04wazj0f72snp86htau5gy0ejaj",
-	},
-	{
-		OldAccStr: "firma1x0lqg5vcynse3r6mug8vteu77cqyaqkgsegn3g",
-		NewAccStr: "firma1fzupf3r5gk505ddt4qpms0gsa7e09j68kzexzl",
-		NewValStr: "firmavaloper1fzupf3r5gk505ddt4qpms0gsa7e09j68g3jaz3",
-	},
-	{
 		OldAccStr: "firma1cztkfn8alkj69d79hvygvdueuvyxem7zj0pt9h",
 		NewAccStr: "firma1c2d74r4fgav52tuq9w9e3sz6d7dk2hx6sgvarp",
-		NewValStr: "firmavaloper1c2d74r4fgav52tuq9w9e3sz6d7dk2hx6wm8xr0",
+		NewValStr: "firmavaloper1nssuz67am2uwc2hjgvphg0fmj3k9l6cxy8ha9j",
 	},
 	{
 		OldAccStr: "firma1vqr622jkes6xmh2cxqa7a04dp6seyme3k9dqlm",
 		NewAccStr: "firma1c2d74r4fgav52tuq9w9e3sz6d7dk2hx6sgvarp",
-		NewValStr: "firmavaloper1c2d74r4fgav52tuq9w9e3sz6d7dk2hx6wm8xr0",
-	},
-	{
-		OldAccStr: "firma1pa8yzyytgfg5hk75gmhkpcq6kds57ak3mhx48z",
-		NewAccStr: "firma1wer4d5h7rs0knhcf7jcgevjqymmx3szyf8ptgc",
-		NewValStr: "firmavaloper1wer4d5h7rs0knhcf7jcgevjqymmx3szyh52sgk",
-	},
-	{
-		OldAccStr: "firma1d84pmnumnsh80v74lta0vnpd476ncp4pjnuklr",
-		NewAccStr: "firma1wer4d5h7rs0knhcf7jcgevjqymmx3szyf8ptgc",
-		NewValStr: "firmavaloper1wer4d5h7rs0knhcf7jcgevjqymmx3szyh52sgk",
-	},
-	{
-		OldAccStr: "firma1u7f4kz740jcr5skq9xnpw8568uugtja5zuau7z",
-		NewAccStr: "firma1q3vad3h69pe8xe4lk3uqdpy87tj3hydyqhtfzw",
-		NewValStr: "firmavaloper1q3vad3h69pe8xe4lk3uqdpy87tj3hydy7yqjzq",
-	},
-	{
-		OldAccStr: "firma1paujadj4fxmqrzwrnhqacuk5dst3fkhc2m9kdj",
-		NewAccStr: "firma1q3vad3h69pe8xe4lk3uqdpy87tj3hydyqhtfzw",
-		NewValStr: "firmavaloper1q3vad3h69pe8xe4lk3uqdpy87tj3hydy7yqjzq",
-	},
-	{
-		OldAccStr: "firma16treuwa6d64xs4r4sshurw75363pwp4nefmpsw",
-		NewAccStr: "firma1qy5rfrzn9jtclncyx3c9fp5ea85ak0uv4pvudj",
-		NewValStr: "firmavaloper1qy5rfrzn9jtclncyx3c9fp5ea85ak0uvtj88du",
-	},
-	{
-		OldAccStr: "firma1q90jttne2mje828mfh3nmfw8kucdsfwfywlr2p",
-		NewAccStr: "firma1qy5rfrzn9jtclncyx3c9fp5ea85ak0uv4pvudj",
-		NewValStr: "firmavaloper1qy5rfrzn9jtclncyx3c9fp5ea85ak0uvtj88du",
-	},
-	{
-		OldAccStr: "firma1tjc2j8aq59s2t9waakacj9xc76yx97t7x8lvke",
-		NewAccStr: "firma1qy5rfrzn9jtclncyx3c9fp5ea85ak0uv4pvudj",
-		NewValStr: "firmavaloper1qy5rfrzn9jtclncyx3c9fp5ea85ak0uvtj88du",
+		NewValStr: "firmavaloper1nssuz67am2uwc2hjgvphg0fmj3k9l6cxy8ha9j",
 	},
 }
 
@@ -116,7 +62,7 @@ func CreateV0_5_1UpgradeHandler(
 		logger.Info(fmt.Sprintf("post migrate version map: %v", versionMap))
 
 		// ==== Custom migration: account Moves and delegations ====
-		if ctx.ChainID() == "colosseum-1" {
+		if ctx.ChainID() == "imperium-4" { // testnet-only version
 			bondDenom := appparams.DefaultBondDenom
 
 			// Withdraw commission of all old validators
